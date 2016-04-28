ca619148eb226b68d15969773b5d9b87761294a5
yayoiukai <yayoi.ukai@gmail.com>
2016-04-27 22:04:04 -0700

    improve the quickstart

--- signalserver/urls.py
+++ signalserver/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from fileuploads import views
 
 urlpatterns = [
-     url(r'^fileuploads/', include('fileuploads.urls')),
+    url(r'^$', views.list, name='list'),
+    url(r'^fileuploads/', include('fileuploads.urls')),
     url(r'^admin/', admin.site.urls),
 ]
