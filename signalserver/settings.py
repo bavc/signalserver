1e7aa2bd3405c061e1fb0c07cf84780470a14c5b
yayoiukai <yayoi.ukai@gmail.com>
2016-08-15 12:19:28 -0700

    Changing the UI

--- signalserver/settings.py
+++ signalserver/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'signalserver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
