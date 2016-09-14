520b5bb9a8f8eb5b11d113048ba26db50e462556
yayoiukai <yayoi.ukai@gmail.com>
2016-09-13 19:04:43 -0700

    Adding user registration and login

--- fileuploads/forms.py
+++ fileuploads/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from operations.models import Configuration
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class FileNameForm(forms.Form):
@@ -40,4 +42,33 @@ class VideoForm(forms.Form):
         widget=forms.ClearableFileInput(attrs={'multiple': True}),
         label='Select a file',
         help_text='max. 42 megabytes'
+    )
+
+
+class UserForm(forms.Form):
+    username = forms.CharField(
+        label='Username',
+        required=False
+    )
+    password = forms.CharField(
+        label='password',
+        widget=forms.PasswordInput,
+        required=True
+    )
+    password2 = forms.CharField(
+        label='confirm password',
+        widget=forms.PasswordInput,
+        required=True
+    )
+    email = forms.EmailField(
+        label='email',
+        required=True
+    )
+    first_name = forms.CharField(
+        label='First name',
+        required=False
+    )
+    last_name = forms.CharField(
+        label='Last name',
+        required=False
     )
