4448652413fc60302e8d2d37ef6205044cbd1c55
yayoiukai <yayoi.ukai@gmail.com>
2016-11-17 22:49:31 -0800

    removing undeeded code and fix for task.py

--- signalserver/settings.py
+++ signalserver/settings.py
@@ -171,7 +171,7 @@ STATICFILES_FINDERS = (
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'bavc.signalserver@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'password1234BAVC'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
